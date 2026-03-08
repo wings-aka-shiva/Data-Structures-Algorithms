8f2a0249417e27d02c06bea25e4b7cb5033e9bf7
wing_shiva <shivatirupathi10@gmail.com>
2026-03-08 18:57:08 +1100

    Implemented BFS traversal with support for disconnected graph components

---
+++ BFSGraphTraversal.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class BFSGraphTraversal {
+
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    // Create Graph
+    public static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+    }
+
+    // BFS for one component
+    public static void bfs(ArrayList<Edge>[] graph, boolean[] vis, int start) {
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+
+        while (!q.isEmpty()) {
+
+            int curr = q.poll();
+
+            if (!vis[curr]) {
+
+                System.out.print(curr + " ");
+                vis[curr] = true;
+
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.offer(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int V = 7;
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        createGraph(graph);
+
+        boolean vis[] = new boolean[V];
+
+        // Handle disconnected graphs
+        for (int i = 0; i < V; i++) {
+
+            if (!vis[i]) {
+                bfs(graph, vis, i);
+            }
+        }
+    }
+}
